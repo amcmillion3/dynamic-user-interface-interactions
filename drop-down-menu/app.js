faff5bd1518774a901c8dae0d102f5ede76458a4
Andrew McMillion <amcmillion3@gmail.com>
2021-10-26 12:24:42 -0400

    Add styling and functionality

--- drop-down-menu/app.js
+++ drop-down-menu/app.js
@@ -1,4 +1,15 @@
-const clickDropButton = document.querySelector('.click-dropdown-button');
-const clickDropContent = document.querySelector('.click-drop-content');
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]');
+    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
 
+    let currentDropdown;
+    if(isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    };
 
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if(dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+    });
+});
